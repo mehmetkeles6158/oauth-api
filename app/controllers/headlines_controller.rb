a225aaf3bc6f03da984ce6fad839d02ec2ef65b2
Mehmet <mehmetk6158@gmail.com>
2021-12-01 21:01:00 -0500

    add api key with encrypt...

---
+++ app/controllers/headlines_controller.rb
@@ -0,0 +1,7 @@
+class HeadlinesController < ApplicationController
+  def index
+    response = HTTP.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=#{Rails.application.credentials.news_api_key}")
+
+    render json: response.parse
+  end
+end
